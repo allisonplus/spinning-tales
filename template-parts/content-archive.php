550574596f375d3814b4012296284fdb2afc9cdb
Allison Tarr <hello@allisontarr.com>
2018-05-23 22:48:28 -0400

    📚archive: tags/categories template part + styling

---
+++ template-parts/content-archive.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template part for displaying archive posts loop.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Spinning Tales
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'loop-single-post' ); ?>>
+
+	<div class="feat-img-wrapper">
+		<img src="<?php echo esc_url( cps_featured_fallback( 'featured-blog' ) ); ?>" alt="alt="<?php esc_html_e( 'Featured image for ', 'cps' ); ?><?php echo the_title(); ?>">
+	</div>
+
+	<div class="post-info">
+		<div class="meta-data">
+			<span class="entry-date"></i><?php the_date( 'F jS, Y', '<p>', '</p>' ); ?></span>
+		</div> <!--/.meta-data-->
+		<h2 class="single-post-title"><a href="<?php the_permalink(); ?>" title="Permalink to: <?php esc_attr( the_title_attribute() ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+	</div> <!--/.post-info-->
+
+</article><!-- #post-## -->
